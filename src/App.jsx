9648d20ec71526a1edd36fbf06721b77cf7c7799
Codingheart <codingheart@web.de>
2025-05-28 18:34:28 +0200

    Clean up code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import { uid } from 'uid';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import { NavBar } from './components/NavBar';
 import { Header } from './components/Header';
@@ -24,21 +23,13 @@ export function App() {
   }, []); //once, if app is mounted
 
   async function addPost(newItem) {
-    // setPosts([...posts, { id: uid(), ...newItem }]);
     const newId = await sendAddPost(newItem);
     console.log('App.jsx - addPost - newId: ');
     console.log(newId);
     loadPosts();
   }
   async function updatePost(id, updatedItem) {
-    //updatedItem ist entweder { votes: votes + 1 } oder { votes: votes - 1 }
-    // setPosts(
-    //   posts.map((item) =>
-    //     item._id === id ? { ...item, ...updatedItem } : item
-    //   )
-    // );
     const updated = await sendPostUpdate(id, updatedItem);
-
     console.log('App.jsx - updatePost - updated: ');
     console.log(updated);
 
@@ -50,11 +41,7 @@ export function App() {
       <Header />
       <NavBar />
       <Routes>
-        <Route
-          path='/'
-          // element={<AllPosts posts={posts} updatePost={updatePost} />}
-          element={<AllPosts posts={posts} />} //updatePost wird jetzt von PostDetails übernommen
-        />
+        <Route path='/' element={<AllPosts posts={posts} />} />
         <Route path='/add-post' element={<AddPost addPost={addPost} />} />
         <Route
           path='/posts/:id'
